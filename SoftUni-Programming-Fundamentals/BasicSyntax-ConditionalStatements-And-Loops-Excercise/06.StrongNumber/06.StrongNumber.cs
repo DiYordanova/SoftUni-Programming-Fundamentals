f25e96af25c171fc67b928e3ae3bb9cf8df483ed
Dilyana Yordanova <d-iordanova@mail.bg>
2022-02-16 20:33:50 +0200

    Added solution.

---
+++ SoftUni-Programming-Fundamentals/BasicSyntax-ConditionalStatements-And-Loops-Excercise/06.StrongNumber/06.StrongNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int originNumber = number;
+            while (number > 0)
+            {
+                int lastDigit =number % 10;
+                int factoriel = 1;
+                for (int i = 1; i <= lastDigit; i++)
+                {
+                    factoriel = factoriel * i;
+                }
+
+                sum += factoriel;
+                number /= 10;
+            }
+
+            if (originNumber == sum)
+            {
+                Console.WriteLine("yes");
+            }
+
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
