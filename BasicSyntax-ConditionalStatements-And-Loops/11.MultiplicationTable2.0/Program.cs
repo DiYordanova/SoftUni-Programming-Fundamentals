65287639d0077c9b8a6be0d92109c66dfe21e881
Dilyana Yordanova <d-yordanova@mail.bg>
2022-02-17 01:07:48 +0200

    Added solution.

---
+++ BasicSyntax-ConditionalStatements-And-Loops/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10.MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                if (m > 10)
+                {
+                    Console.WriteLine($"{n} X {m} = {n * m}");
+                    return;
+                }
+                else if (i >= m)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
+
