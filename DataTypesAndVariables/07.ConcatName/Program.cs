37642d231fc525b870969d07e2e5dea0620d5d27
Dilyana Yordanova <d-yordanova@mail.bg>
2022-02-26 17:47:52 +0200

    Added solution.

---
+++ DataTypesAndVariables/07.ConcatName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.ConcatName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFirst = Console.ReadLine();
+            string nameSecond = Console.ReadLine();
+            string symbol = Console.ReadLine();
+            Console.WriteLine($"{nameFirst}{symbol}{nameSecond}");
+        }
+    }
+}
