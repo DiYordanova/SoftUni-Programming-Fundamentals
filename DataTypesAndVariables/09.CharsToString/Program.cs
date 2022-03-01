b53244b5f0e22bb8fb7f174c1e58ac4bdf127a11
Dilyana Yordanova <d-yordanova@mail.bg>
2022-03-01 17:13:38 +0200

    Added solution.

---
+++ DataTypesAndVariables/09.CharsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09.CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char numberOne = char.Parse(Console.ReadLine());
+            char numberTwo = char.Parse(Console.ReadLine());
+            char numberThree = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{numberOne}{numberTwo}{numberThree}");
+        }
+    }
+}
