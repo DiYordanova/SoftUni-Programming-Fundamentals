0a120d4c40aefdfc4a9de14e000d6582c8be282a
Dilyana Yordanova <d-yordanova@mail.bg>
2022-03-10 16:22:39 +0200

    Added solution.

---
+++ DataTypeAndVariables-Excersise/07.WaterOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int litre = int.Parse(Console.ReadLine());
+                if (sum + litre > 255)
+                {
+                    Console.WriteLine($"Insufficient capacity!");                    
+                }
+
+                else
+                {
+                sum += litre;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
