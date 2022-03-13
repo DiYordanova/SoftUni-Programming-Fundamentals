c30439422cdede29d1d764088c106abc169619d1
Dilyana Yordanova <d-yordanova@mail.bg>
2022-03-13 21:32:50 +0200

    Added solution.

---
+++ DataTypeAndVariables-Excersise/08.BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string biggestKeg = "";
+            double biggestVolume = double.MinValue;
+            for (int i = 0; i < number; i++)
+            {
+                string nameKeg = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume > biggestVolume)
+                {
+                    biggestVolume = volume;
+                    biggestKeg = nameKeg;
+                }
+            }
+
+            Console.WriteLine($"{biggestKeg}");
+        }
+    }
+}
