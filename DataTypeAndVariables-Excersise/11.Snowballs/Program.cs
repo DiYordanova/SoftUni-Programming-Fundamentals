894a421f01b052ae52682a43e429c59d3b87e75d
Dilyana Yordanova <d-yordanova@mail.bg>
2022-03-24 16:06:53 +0200

    Added solution.

---
+++ DataTypeAndVariables-Excersise/11.Snowballs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11.Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSnowballs = int.Parse(Console.ReadLine());
+            BigInteger snowballMax = -1;
+            string result = "";
+            for (int i = 0; i < numberSnowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+                if (snowballValue > snowballMax)
+                {
+                    snowballMax = snowballValue;
+                    result = $"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})";
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
