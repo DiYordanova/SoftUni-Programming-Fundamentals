c321f1ee74d3c840bc2e83635ede68796fed1e5e
Dilyana Yordanova <d-yordanova@mail.bg>
2022-03-31 01:33:21 +0300

    Added solution.

---
+++ Array/06.EvenAndOdd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEven = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] % 2 == 0)
+                {
+                    int currentSumEven = number[i];
+                    sumEven += currentSumEven;
+                }
+            }
+
+            int sumOdd = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] % 2 != 0)
+                {
+                    int currentSumOdd = number[i];
+                    sumOdd += currentSumOdd;
+                }
+            }
+
+            int rezult = sumEven - sumOdd;
+            Console.WriteLine(rezult);
+        }
+    }
+}
