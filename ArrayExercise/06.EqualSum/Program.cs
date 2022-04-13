db901323f931db379edbe8ddd5045e98497a0492
Dilyana Yordanova <d-yordanova@mail.bg>
2022-04-13 08:46:34 +0300

    Added solution.

---
+++ ArrayExercise/06.EqualSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+            .ToArray();
+
+            bool flag = false;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int leftSum = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += number[j];
+                }
+
+                int rightSum = 0;
+                for (int k = i + 1; k < number.Length; k++)
+                {
+                    rightSum += number[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
