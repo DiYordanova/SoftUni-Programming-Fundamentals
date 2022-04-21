d6ea64029aadafdceed9628a8d50d451852b8443
Dilyana Yordanova <d-yordanova@mail.bg>
2022-04-21 18:50:58 +0300

    Added solution.

---
+++ ArrayExercise/07.MxSequence/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MxSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int countMax = 0;
+            int bestNumber = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                int count = 1;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int rightNumber = numbers[j];
+                    if (currentNumber == rightNumber)
+                    {
+                        count++;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                    if (count > countMax)
+                    {
+                        countMax = count;
+                        bestNumber = currentNumber;
+                    }
+                }
+            }
+
+            for (int i = 0; i < countMax; i++)
+            {
+                Console.Write($"{bestNumber} ");
+            }
+        }
+    }
+}
+
